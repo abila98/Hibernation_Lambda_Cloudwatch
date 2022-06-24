5af0a72ad805b0bdec104099d7f15187f3f464c5
abila98 <41573378+abila98@users.noreply.github.com>
2022-06-24 18:14:25 +0530

    Create start_instance.py

---
+++ start_instance.py
@@ -0,0 +1,25 @@
+import boto3  
+client = boto3.client('ec2') 
+instances_id = []
+
+def lambda_handler(event, context):
+    filters = [{  
+    'Name': "tag:Scheduled",
+    'Values': ['True']
+    }]
+    response = client.describe_instances(Filters=filters)
+    print(response)       # in logs
+    
+    
+    for instanceid in response["Reservations"]:
+        for id in instanceid["Instances"]:
+            print(id["InstanceId"])
+            
+            temp = id["InstanceId"]
+            client.start_instances(InstanceIds=[temp])
+            
+            instances_id.append(id["InstanceId"])
+            
+    
+    return instances_id     #comes in response
+    
